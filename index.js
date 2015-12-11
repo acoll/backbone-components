ce84143727605fcd8a54d810d7825b22236c884e
Adam <adamcoll.ac@gmail.com>
2015-12-11 16:35:44 -0500

    small updates with logging and preventing component stealing by parents

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var components = {};
 module.exports = {
 	components: components,
 	register: function (name, Component) {
+		if(name.indexOf('-component') > 0) throw new Error('-component is already appended to the end of the name you pass in.');
+
+		name = name + '-component';
+
 		if(components[name]) {
 			logger.warn('Skipping `' + name + '` It is already registered.');
 		}
@@ -12,13 +16,24 @@ module.exports = {
 		logger.info('`' + name + '` registered');
 	},
 	init: function (Backbone) {
+
+		logger.info('Initializing with Backbone instance:', Backbone);
+
 		var orig = Backbone.View;
 		Backbone.View = Backbone.View.extend({
 			constructor: function () {
+				// logger.debug('Applying custom component logic to View', this);
 				orig.apply(this, arguments);
 
 				this.on('render', function () {
-					this.$el.find('*').each((index, el) => {
+					var model = this.model;
+					var _this = this;
+					this.$el.find('*').each(function(index, el) {
+
+						// skip if already initialized by lower component
+						if(el.attributes.getNamedItem('__view') && el.attributes.getNamedItem('__view').value) {
+							return;
+						}
 
 						var componentClass = components[el.tagName.toLowerCase()];
 
@@ -28,19 +43,22 @@ module.exports = {
 							for(var i = 0; i < el.attributes.length; i++) {
 								var attrib = el.attributes[i];
 								if(attrib.value.indexOf('$') === 0) 
-									opts[attrib.name] = getValue(this.model.toJSON(), attrib.value);
+									opts[attrib.name] = getValue(model, attrib.value, el, _this);
 							 	else 
 							 		opts[attrib.name] = attrib.value;
 							}
 
-							logger.info('Creating component:', el.tagName.toLowerCase(), 'with opts', opts);
+							logger.info('Creating component:', el.tagName.toLowerCase(), 'with opts', opts, 'for view:', _this.cid);
 							var comp = new componentClass(opts);
 							try {
 								comp.render();
+								comp.$el.attr('__view', _this.cid);
 							} catch(err) {
 								logger.error('Error rendering', el, err, err.stack);
 							}
 							if(comp.triggerMethod) comp.triggerMethod('show');
+						} else {
+							if(el.tagName.toLowerCase().indexOf('-component') > 0) logger.warn('No component registered for', el.tagName.toLowerCase());
 						}
 
 					});
@@ -50,20 +68,28 @@ module.exports = {
 	}
 };
 
-function getValue(data, expr) {
+function getValue(model, expr, el, view) {
+	var data = {};
+	if(model && model.toJSON) data = model.toJSON();
 
-	expr = expr.substring(2, expr.length - 1);
+
+	var parsedExpr = expr.substring(2, expr.length - 1);
 
 	var fnExpr = [
 		'(function (',
 		Object.keys(data).join(','),
-		'){ return ' + expr + '; })',
+		'){ return ' + parsedExpr + '; })',
 		'(' + Object.keys(data).map(function (p) { return 'data["' + p + '"]'; }).join(',') + ')'
 	].join('');
 
-	var result = eval(fnExpr);
+	var result = undefined;
+	try {
+		result = eval(fnExpr);
+	} catch(err) {
+		logger.error('Error evaluating:', expr, 'for el:', el, 'inside view:', view.cid, view.name);
+		logger.error(err);
 
-	console.log(fnExpr, '=', result);
+	}
 
 	return result;
 }
